8fc6bf47baab62dcc6444cdc833845a929866c74
conan0220 <conanhuang8382@gmail.com>
2023-02-09 22:34:23 +0800

    Add third-party libraries, Symengine and GMP.

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,4 +4,19 @@ int main() {
     Application app;
 }
 
+// #include <iostream>
+// #include <symengine/expression.h>
+// #include <symengine/symbol.h>
+// #include <symengine/solve.h>
 
+// using namespace SymEngine;
+
+// int main() {
+//     RCP<const Symbol> x = symbol("x");
+//     RCP<const Basic> equation = add(add(pow(x, integer(2)), mul(x, integer(3))), integer(2));
+//     RCP<const Set> solutions = solve(equation, x);
+//     std::cout << solutions->get_args().size() << std::endl;     // size
+//     std::cout << solutions->get_args() << std::endl;            // all datas
+//     std::cout << *solutions->get_args()[0] << std::endl;        // data1
+//     std::cout << *solutions->get_args()[1] << std::endl;        // data2
+// }
